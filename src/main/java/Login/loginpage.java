4781956d14026b9b304b23c0909fa05a0720010a
shiva <sivaram.bagadi@gmail.com>
2026-03-23 11:47:56 -0500

    first commit thru eclipse

---
+++ src/main/java/Login/loginpage.java
@@ -0,0 +1,13 @@
+package Login;
+
+public class loginpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Login functionality is working fine");
+		
+
+	}
+
+}
