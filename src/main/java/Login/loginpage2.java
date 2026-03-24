a0aec468a6cec8e07beabe8757b15a2616dbc9c1
shiva <sivaram.bagadi@gmail.com>
2026-03-24 18:13:17 -0500

    login page 2 added to access inbox page

---
+++ src/main/java/Login/loginpage2.java
@@ -0,0 +1,13 @@
+package Login;
+
+public class loginpage2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Login functionality is working fine");
+		
+
+	}
+
+}
